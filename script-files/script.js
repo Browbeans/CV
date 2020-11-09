9a12ace1bfd07ad12288bafd19c6d0ae503b58fb
Browbeans <oliverjohansson3@gmail.com>
2020-11-09 12:30:00 +0100

    add rock paper scissor game

--- script-files/script.js
+++ script-files/script.js
@@ -2,8 +2,29 @@ window.addEventListener('load', main)
 const aboutMe = document.getElementById('aboutMe')
 const projects = document.getElementById('projects')
 const boxes = document.getElementById('boxes')
+const selectionButtons = document.querySelectorAll('[data-selection]')
+const finalColumn = document.querySelector('[data-final-column]')
+const oliverScore = document.querySelector('[data-oliver-score]')
+const userScore = document.querySelector('[data-user-score]')
+
+const SELECTIONS = [
+    {
+        name: 'rock',
+        emoji: '✊',
+        beats: 'scissors',
+    },
+    {
+        name: 'paper',
+        emoji: '✋',
+        beats: 'rock',
+    },
+    {
+        name: 'scissor',
+        emoji: '✌️',
+        beats: 'paper',
+    }
+]
 let i = 0; 
-let o = 0;  
 
 
 function main (){
@@ -20,10 +41,6 @@ function typeWriter() {
         setTimeout(typeWriter, speed)
     }
 } 
-
-/*window.onresize = function () {
-    if 
-}*/
 
 function addEventListeners () {
     aboutMe.onmouseover = makeAboutMeBigger
@@ -48,4 +65,46 @@ function makeProjectsBigger() {
 
 function makeProjectsSmaller() {
     projects.style.height = "3rem"
+}
+
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+        const selectionName = selectionButton.dataset.selection
+        const selection = SELECTIONS.find(selection => selection.name === selectionName)
+        makeSelection(selection)
+            
+    })
+})
+
+function makeSelection(selection) {
+    const oliverSelection = randomSelection()
+    const userWinner = isWinner(selection, oliverSelection)
+    const oliverWinner = isWinner(oliverSelection, selection)
+    
+    addSelectionResult(oliverSelection, oliverWinner)
+    addSelectionResult(selection, userWinner)
+
+    if(userWinner)incrementScore(userScore)
+    if(oliverWinner)incrementScore(oliverScore)
+}
+
+function incrementScore(Score){
+    Score.innerText = parseInt(Score.innerText) + 1
+}
+
+function addSelectionResult(selection, winner) {
+    const div = document.createElement('div')
+    div.innerText = selection.emoji
+    div.classList.add('result-selection')
+    if(winner) div.classList.add('winner')
+    finalColumn.after(div)
+}
+
+function isWinner (selection, opponentSelection) {
+    return selection.beats === opponentSelection.name
+}
+
+function randomSelection() {
+    const randomIndex =  Math.floor(Math.random() * SELECTIONS.length)
+    return SELECTIONS [randomIndex]
 }
