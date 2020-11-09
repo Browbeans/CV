8a3f6f8e0eae39805346eb9e5fc189fcf1692966
Browbeans <oliverjohansson3@gmail.com>
2020-11-09 10:36:03 +0100

    add better structure to functions

--- script-files/script.js
+++ script-files/script.js
@@ -1,23 +1,51 @@
 window.addEventListener('load', main)
-const aboutMe = document.getElementById("aboutMe")
-const projects = document.getElementById("projects")
+const aboutMe = document.getElementById('aboutMe')
+const projects = document.getElementById('projects')
+const boxes = document.getElementById('boxes')
+let i = 0; 
+let o = 0;  
+
 
 function main (){
-
+    addEventListeners()
+    typeWriter()
 }
 
-aboutMe.onmouseover = function () {
-    aboutMe.style.height = "20rem"  
+function typeWriter() {
+    const speed = 50
+    let p = 'Take a look around here to find out more about me and my knowledge'
+    if(i < p.length) {
+        document.getElementById('welcome').innerHTML += p.charAt(i)
+        i++
+        setTimeout(typeWriter, speed)
+    }
+} 
+
+/*window.onresize = function () {
+    if 
+}*/
+
+function addEventListeners () {
+    aboutMe.onmouseover = makeAboutMeBigger
+    aboutMe.onmouseout = makeAboutMeSmaller
+
+    projects.onmouseover = makeProjectsBigger
+    projects.onmouseout = makeProjectsSmaller
 }
 
-aboutMe.onmouseout = function() {
+
+function makeAboutMeBigger(){
+    aboutMe.style.height = "30rem"
+}
+
+function makeAboutMeSmaller() {
     aboutMe.style.height = "3rem"
 }
 
-projects.onmouseover = function () {
-    projects.style.height = "20rem"  
+function makeProjectsBigger() {
+    projects.style.height = "20rem"    
 }
 
-projects.onmouseout = function() {
+function makeProjectsSmaller() {
     projects.style.height = "3rem"
 }
