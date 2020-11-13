48a349f629c794cd1771dc05e5980a9f372ad6a2
Browbeans <oliverjohansson3@gmail.com>
2020-11-13 15:48:51 +0100

    Fixed JSdoc

--- script-files/game.js
+++ script-files/game.js
@@ -1,10 +1,10 @@
-//** Get elemenst from DOM and put them in global variables */
+/** Get elemenst from DOM and put them in global variables */
 const selectionButtons = document.querySelectorAll('[data-selection]')
 const finalColumn = document.getElementById('finalcolumn')
 const oliverScore = document.getElementById('oliverscore')
 const userScore = document.getElementById('userscore')
 
-//** Creates an array with objects for the selections in rock paper scissor */
+/** Creates an array with objects for the selections in rock paper scissor */
 const SELECTIONS = [
     {
         name: 'rock',
@@ -35,7 +35,9 @@ selectionButtons.forEach(selectionButton => {
     })
 })
 
-//** This function will read all selections and decide winner and determine wich to call upon*/
+/** This function will read all selections and decide winner and determine wich to call upon
+ * @param {Param} selection Gets user choice from previous function
+*/
 function makeSelection(selection) {
     const oliverSelection = randomSelection()
     const userWinner = isWinner(selection, oliverSelection)
@@ -49,12 +51,17 @@ function makeSelection(selection) {
 }
 
 
-//** This function will increment the current score based on who wins in the game */
+/** This function will increment the current score based on who wins in the game
+ * @param {Param} Score Takes the score from previous function
+ */
 function incrementScore(Score){
     Score.innerText = parseInt(Score.innerText) + 1
 }
 
-//** This function will change styling for the winner icon */
+/** This function will change styling for the winner icon 
+ * @param {Param} selection Users selection
+ * @param {Param} winner Will decide based on selection how to style
+*/
 function addSelectionResult(selection, winner) {
     const div = document.createElement('div')
     div.innerText = selection.emoji
@@ -63,12 +70,14 @@ function addSelectionResult(selection, winner) {
     finalColumn.after(div)
 }
 
-//** Function that enable a winner, based on "beats" in the object */
+/** Function that enable a winner, based on "beats" in the object 
+ * @param {Param} opponentSelection Computers selection 
+*/
 function isWinner (selection, opponentSelection) {
     return selection.beats === opponentSelection.name
 }
 
-//** Function that will generate the computers "choice" based on math random  */
+/** Function that will generate the computers "choice" based on math random  */
 function randomSelection() {
     const randomIndex =  Math.floor(Math.random() * SELECTIONS.length)
     return SELECTIONS [randomIndex]
--- script-files/script.js
+++ script-files/script.js
@@ -1,10 +1,6 @@
-//** Function that will start the main function when the page loads in */
+/** Function that will start the main function when the page loads in */
 window.addEventListener('load', main)
 
-//** Get elemenst from DOM and put them in global variables */
-const aboutMe = document.getElementById('aboutMe')
-const projects = document.getElementById('projects')
-const boxes = document.getElementById('boxes')
 
 /** Sets the value of i(index) to 0  */
 let i = 0; 
@@ -16,7 +12,7 @@ function main (){
 }
 
 
-//** Function typewriter that creates a visual effect of a typewriter */
+/** Function typewriter that creates a visual effect of a typewriter */
 function typeWriter() {
     const speed = 50
     let p = 'Take a look around here to find out more about me and my knowledge'
@@ -27,31 +23,33 @@ function typeWriter() {
     }
 }
 
-//** Function that will "notice" users interactivity with the page, and triggers function based on users "action"  for example (mouseclick) etc*/
+/** Function that will "notice" users interactivity with the page, and triggers function based on users "action" for example (mouseclick) etc*/
 function addEventListeners () {
+    let aboutMe = document.getElementById('aboutMe')
     aboutMe.onmouseover = makeAboutMeBigger
     aboutMe.onmouseout = makeAboutMeSmaller
-
+    
+    let projects = document.getElementById('projects')
     projects.onmouseover = makeProjectsBigger
     projects.onmouseout = makeProjectsSmaller
 }
 
-//** Will give div height of 30rem when user hover over it with the mouse */
+/** Will give div height of 30rem when user hover over it with the mouse */
 function makeAboutMeBigger(){
     aboutMe.style.height = "30rem"
 }
 
-//** Will give a div the height 3rem when the user isnt hover over it*/
+/** Will give a div the height 3rem when the user isnt hover over it*/
 function makeAboutMeSmaller() {
     aboutMe.style.height = "3rem"
 }
 
-//** Will give div height of 30rem when user hover over it with the mouse */
+/** Will give div height of 30rem when user hover over it with the mouse */
 function makeProjectsBigger() {
     projects.style.height = "20rem"    
 }
 
-//** Will give a div the height 3rem when the user isnt hover over it*/
+/** Will give a div the height 3rem when the user isnt hover over it*/
 function makeProjectsSmaller() {
     projects.style.height = "3rem"
 }
