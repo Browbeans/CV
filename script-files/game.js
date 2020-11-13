bb3d77d3ce9ad55937e082b12abf24226b7f3d02
Browbeans <oliverjohansson3@gmail.com>
2020-11-13 12:32:19 +0100

    Add jsdoc

--- script-files/game.js
+++ script-files/game.js
@@ -1,8 +1,10 @@
+//** Get elemenst from DOM and put them in global variables */
 const selectionButtons = document.querySelectorAll('[data-selection]')
 const finalColumn = document.getElementById('finalcolumn')
 const oliverScore = document.getElementById('oliverscore')
 const userScore = document.getElementById('userscore')
 
+//** Creates an array with objects for the selections in rock paper scissor */
 const SELECTIONS = [
     {
         name: 'rock',
@@ -21,15 +23,19 @@ const SELECTIONS = [
     }
 ]
 
+/**
+ * Function that will take in the users choice
+ * @param {Param} selectionButton 
+ */
 selectionButtons.forEach(selectionButton => {
     selectionButton.addEventListener('click', e => {
         const selectionName = selectionButton.dataset.selection
         const selection = SELECTIONS.find(selection => selection.name === selectionName)
-        makeSelection(selection)
-            
+        makeSelection(selection)       
     })
 })
 
+//** This function will read all selections and decide winner and determine wich to call upon*/
 function makeSelection(selection) {
     const oliverSelection = randomSelection()
     const userWinner = isWinner(selection, oliverSelection)
@@ -42,10 +48,13 @@ function makeSelection(selection) {
     if(oliverWinner)incrementScore(oliverScore)
 }
 
+
+//** This function will increment the current score based on who wins in the game */
 function incrementScore(Score){
     Score.innerText = parseInt(Score.innerText) + 1
 }
 
+//** This function will change styling for the winner icon */
 function addSelectionResult(selection, winner) {
     const div = document.createElement('div')
     div.innerText = selection.emoji
@@ -54,10 +63,12 @@ function addSelectionResult(selection, winner) {
     finalColumn.after(div)
 }
 
+//** Function that enable a winner, based on "beats" in the object */
 function isWinner (selection, opponentSelection) {
     return selection.beats === opponentSelection.name
 }
 
+//** Function that will generate the computers "choice" based on math random  */
 function randomSelection() {
     const randomIndex =  Math.floor(Math.random() * SELECTIONS.length)
     return SELECTIONS [randomIndex]
--- script-files/script.js
+++ script-files/script.js
@@ -1,16 +1,22 @@
+//** Function that will start the main function when the page loads in */
 window.addEventListener('load', main)
+
+//** Get elemenst from DOM and put them in global variables */
 const aboutMe = document.getElementById('aboutMe')
 const projects = document.getElementById('projects')
 const boxes = document.getElementById('boxes')
 
+/** Sets the value of i(index) to 0  */
 let i = 0; 
 
-
+/** Main function that will start the program*/
 function main (){
     addEventListeners()
     typeWriter()
 }
 
+
+//** Function typewriter that creates a visual effect of a typewriter */
 function typeWriter() {
     const speed = 50
     let p = 'Take a look around here to find out more about me and my knowledge'
@@ -21,6 +27,7 @@ function typeWriter() {
     }
 }
 
+//** Function that will "notice" users interactivity with the page, and triggers function based on users "action"  for example (mouseclick) etc*/
 function addEventListeners () {
     aboutMe.onmouseover = makeAboutMeBigger
     aboutMe.onmouseout = makeAboutMeSmaller
@@ -29,19 +36,22 @@ function addEventListeners () {
     projects.onmouseout = makeProjectsSmaller
 }
 
-
+//** Will give div height of 30rem when user hover over it with the mouse */
 function makeAboutMeBigger(){
     aboutMe.style.height = "30rem"
 }
 
+//** Will give a div the height 3rem when the user isnt hover over it*/
 function makeAboutMeSmaller() {
     aboutMe.style.height = "3rem"
 }
 
+//** Will give div height of 30rem when user hover over it with the mouse */
 function makeProjectsBigger() {
     projects.style.height = "20rem"    
 }
 
+//** Will give a div the height 3rem when the user isnt hover over it*/
 function makeProjectsSmaller() {
     projects.style.height = "3rem"
 }
